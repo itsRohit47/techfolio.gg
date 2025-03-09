e566ac45d78af7b3d2a9fcb4bc98f6d5122c3c15
Rohit Bajaj <rohitbajaj13@yahoo.com>
2025-03-09 13:01:18 +1100

    snjbsj

--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -1,7 +1,7 @@
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { type DefaultSession, type NextAuthConfig } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 import { db } from "@/server/db";
 
@@ -41,7 +41,7 @@ export const authConfig = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    Google({
+    GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
